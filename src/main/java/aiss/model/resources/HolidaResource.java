f5884763a91ca9c7cad909c5cf4fcb60a20236ce
alvnavriv <73224236+alvnavriv@users.noreply.github.com>
2021-05-14 13:27:41 +0200

    Delete src directory

--- src/main/java/aiss/model/resources/HolidaResource.java
+++
@@ -1,8 +0,0 @@
-package aiss.model.resources;
-
-public class HolidaResource {
-	private static final String VACACIONES_API_KEY = "6c0586e424b8c7c1c4ae9725a7fd197b663198f3";
-	
-	private String uri = "https://calendarific.com/api/v2/holidays"+"?&api_key="+VACACIONES_API_KEY+"&country="+country+"&year="+year;
-	
-}
